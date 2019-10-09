4c3d513e80bdee98912261494b0c244ead5990d4
solehudindt <solehudindt@gmail.com>
2019-10-09 14:31:58 +0700

    intial dict

---
+++ basic/dictionary.py
@@ -0,0 +1,13 @@
+## Initial dictionary
+
+heroes = {'strength':'axe', 'agility':'monkey king'}
+pahlawan = dict()
+
+print(type(heroes))
+print(type(pahlawan))
+
+print(heroes)
+print(heroes['strength'])
+
+heroes['intelegence'] = 'tinker'
+print(heroes)
