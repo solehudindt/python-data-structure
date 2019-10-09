33448e17a9323f05e2bd1a6933c23ad100db7754
solehudindt <solehudindt@gmail.com>
2019-10-09 14:00:20 +0700

    set++

--- basic/set.py
+++ basic/set.py
@@ -6,3 +6,33 @@ print(type(s))
 print(s)
 s.update([4,5])
 print(s)
+
+## delete set
+s.discard(3)
+print(s)
+
+s.remove(5)
+print(s)
+
+s.pop()
+print(s)
+s.clear()
+print(s)
+
+## Set Operations
+
+a = {1,2,3,4}
+b = {3,4,5,6}
+
+print(a.union(b))
+print(a | b)
+
+print(a.intersection(b))
+print(a & b)
+
+print(a.difference(b))
+print(a - b)
+
+print(a ^ b)
+
+print(a.issubset(b))
