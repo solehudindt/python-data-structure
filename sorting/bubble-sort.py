36eb01513c831a1ca1537f4db52ba624de3f433a
solehudindt <solehudindt@gmail.com>
2019-10-17 01:30:45 +0700

    initial commit bubble_sort

--- sorting/bubble-sort.py
+++ sorting/bubble-sort.py
@@ -1,4 +1,13 @@
-a = list()
+# a = list()
 
-a = [i for i in range(10)]
-print(a)
+# # a = [i for i in range(10)]
+# print(a)
+
+x = [4,1,3,2]
+
+def bubble(list):
+    for i in range(len(list)):
+        for j in range(len(list) - 1,i,-1):
+            print(i, j)
+
+bubble(x)
